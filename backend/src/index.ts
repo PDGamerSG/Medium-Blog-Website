5131c0d57b78c02ec21b5b02545eebfcb658bf1a
PDGamerSG <pallab4586das@gmail.com>
2026-04-13 19:51:01 +0530

    feat: add routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,37 @@
 import { Hono } from 'hono'
+import { prisma } from "../lib/prisma.js";
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
+app.post('/api/v1/signup', async (c) => {
+    //@ts-ignore
+    const dbUrl = c.env.DATABASE_URL;
+    const body = await c.req.json();
+    await prisma.user.create({
+        data:{
+            email: body.email,
+            password: body.password,
+        }
+    })
+
+    return c.text("Hello Hono")
 
 })
 app.post('/api/v1/signin', (c) => {
 
+    return c.text("Hello Hono")
 })
 app.post('/api/v1/blog', (c) => {
 
+    return c.text("Hello Hono")
 })
 app.put('/api/v1/blog', (c) => {
 
+    return c.text("Hello Hono")
 })
 app.get('/api/v1/blog/:id', (c) => {
+    return c.text("Hello Hono")
 
 })
 
