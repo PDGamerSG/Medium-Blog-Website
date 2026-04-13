2d86c984e4a689b52e1f85ba0938a7871d75f749
PDGamerSG <pallab4586das@gmail.com>
2026-04-13 16:00:19 +0530

    refactor: update backend configuration and remove unused files

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.post('/api/v1/signup', (c) => {
+
+})
+app.post('/api/v1/signin', (c) => {
+
+})
+app.post('/api/v1/blog', (c) => {
+
+})
+app.put('/api/v1/blog', (c) => {
+
+})
+app.get('/api/v1/blog/:id', (c) => {
+
+})
+
+
+export default app
